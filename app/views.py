f080ce180151e22370799802b1e783b983b0090f
lida24 <lidalenkova204@gmail.com>
2021-06-16 10:16:39 +0300

    added dz3

--- app/views.py
+++ app/views.py
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render
 from datetime import date
-=======
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.shortcuts import render
 
-from app.models import User, Question, Answer
-from django.core.exceptions import ObjectDoesNotExist
->>>>>>> a0896c2174ea462ef2b0a72a7e51fccaecfd71b2
-
-# from app.models import Article
 from app.models import Profile, Question, Tag, Answer
 from app.models import VoteForAnswer, VoteForQuestion
 
-<<<<<<< HEAD
 def paginate(content_list, request, num_per_page):
     paginator = Paginator(content_list, num_per_page)
 
@@ -26,74 +16,23 @@ def index(request):
     questions = Question.objects.new_questions()
     content = paginate(questions, request, 5)
     return render(request, 'index.html', {'content': content})
-=======
-def paginate(objects_list, request, per_page=5):
-    paginator = Paginator(objects_list, per_page)
-    page_number = request.GET.get('page')
-    return paginator.get_page(page_number)
-
-def index(request):
-    questions = Question.objects.new_questions()
-    page = paginate(questions, request, 5)
-    return render(request, 'index.html', {
-        'page_obj': page
-    })
->>>>>>> a0896c2174ea462ef2b0a72a7e51fccaecfd71b2
-
-def hot(request):
-    questions = Question.objects.hot_questions()
-    page = paginate(questions, request, 5)
-    return render(request, 'index.html', {
-        'page_obj': page
-    })
-
-
-def question(request, pk):
-    try:
-        question = Question.objects.get(id=pk)
-        answers = question.answers.best_answers()
-        page = paginate(answers, request, 5)
-        return render(request, 'question.html', {
-            'question': question,
-            'page_obj': page
-        })
-    except ObjectDoesNotExist:
-        return render(request, '404_not_found.html')
-
-def tag(request, tag):
-    questions = Question.objects.questions_for_tag(tag).all()
-    if len(questions) > 0:
-        page = paginate(questions, request, 5)
-        return render(request, 'tag.html', {
-            'page_obj': page,
-            'tag': tag
-        })
-    else:
-        return render(request, 'blank_page.html')
 
 def ask(request):
-    return render(request, 'ask.html')
+    return render(request, 'ask.html', {})
 
-<<<<<<< HEAD
 def settings(request):
     return render(request, 'settings.html', {})
-=======
 
 def login(request):
     return render(request, 'login.html')
->>>>>>> a0896c2174ea462ef2b0a72a7e51fccaecfd71b2
-
 
 def signup(request):
     return render(request, 'signup.html')
 
-
-<<<<<<< HEAD
 def hot(request):
     questions = Question.objects.hot_questions()
     content = paginate(questions, request, 5)
     return render(request, 'index.html', {'content': content})
-
 
 def tag(request, tag_name):
     questions = Question.objects.tag_questions(tag_name)
@@ -102,12 +41,7 @@ def tag(request, tag_name):
 
 
 def question(request, pk):
-    # .select_related() не работает
     selected_question = Question.objects.single_question(pk)
     selected_answers = Answer.objects.filter(question=selected_question)
     content = paginate(selected_answers, request, 3)
     return render(request, 'question.html', {'question': selected_question, 'content': content})
-=======
-def settings(request):
-    return render(request, 'settings.html')
->>>>>>> a0896c2174ea462ef2b0a72a7e51fccaecfd71b2
--- askmeLenkova/urls.py
+++ askmeLenkova/urls.py
@@ -23,19 +23,9 @@ urlpatterns = [
     path('ask/', views.ask, name='ask'),
     path('hot/', views.hot, name='hot'),
     path('login/', views.login, name='login'),
-<<<<<<< HEAD
     path('question/<int:pk>/', views.question, name='question'),
     path('settings/', views.settings, name='settings'),
     path('signup/', views.signup, name='signup'),
     path('tag/<str:tag_name>', views.tag, name='tag'),
     path('', views.index, name='index')
-=======
-    path('question/<int:pk>/', views.question, name='one_question'),
-    path('signup/', views.signup, name='signup'),
-    path('settings/', views.settings, name='settings'),
-    path('tag/<str:tag>/', views.tag, name='tag'),
-    path('hot/', views.hot, name='hot'),
-    path('', views.index, name='index'),
-
->>>>>>> a0896c2174ea462ef2b0a72a7e51fccaecfd71b2
 ]
